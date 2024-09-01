94337a5648c29ec33811266e1e2a5c0c23fec457
Leonardo Céspedes <lecespedes@estudiantec.cr>
2024-09-01 16:55:27 +0000

    Configure Helmet Using the ‘parent’ helmet() Middleware

--- myApp.js
+++ myApp.js
@@ -4,23 +4,34 @@ const app = express();
 
 const ninetyDaysInSeconds = 90*24*60*60;
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({
-  maxAge: ninetyDaysInSeconds,
-  force: true
-}))
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
+app.use(helmet({
+  frameguard: {action: 'deny'},
+  contentSecurityPolicy: {directives: {
     defaultSrc: ["'self'"],
     scriptSrc: ["'self'", "trusted-cdn.com"]
-  }
-}))
+  }},
+  noCache: true,
+  hsts: {maxAge: ninetyDaysInSeconds, force: true},
+}));
+
+
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// app.use(helmet.hsts({
+//   maxAge: ninetyDaysInSeconds,
+//   force: true
+// }))
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+// app.use(helmet.contentSecurityPolicy({
+//   directives: {
+//     defaultSrc: ["'self'"],
+//     scriptSrc: ["'self'", "trusted-cdn.com"]
+//   }
+// }))
 
 
 
